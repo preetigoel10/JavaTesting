54533d3636dbc3f0f1530be59c85e67f5bfd205e
Preeti Goel <preeti.goel10@gmail.com>
2020-01-23 22:52:56 +0530

    initial commit

---
+++ src/main/java/com/assignment/App.java
@@ -0,0 +1,17 @@
+package com.assignment;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println( "Hello "+name);
+    }
+}
